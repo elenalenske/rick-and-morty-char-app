3eec9d32a8589706ddab129a8d13667a4193c53b
elenarumianceva <arefuthescroob@gmail.com>
2022-04-10 19:40:38 +0300

    deleted unused FormControl

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AllCharacters, Character } from 'src/app/interfaces/characters-interface';
 import { DataService } from 'src/app/services/data.service';
 
